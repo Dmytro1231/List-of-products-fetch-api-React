06f743f21f7108ec6e3459db8aa6d71ee31ed0ab
Dmytro1231 <dimarubalchenk@ukr.net>
2021-03-01 14:18:24 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Lacinia quis vel
+        eros donec ac odio tempor. Morbi tincidunt augue interdum velit euismod
+        in. Ullamcorper a lacus vestibulum sed arcu. Volutpat sed cras ornare
+        arcu dui vivamus arcu felis. Blandit turpis cursus in hac. Integer quis
+        auctor elit sed vulputate. Nisi scelerisque eu ultrices vitae auctor eu.
+        Id consectetur purus ut faucibus pulvinar. Ut porttitor leo a diam
+        sollicitudin tempor id eu nisl. Risus pretium quam vulputate dignissim
+        suspendisse in est ante. Amet est placerat in egestas
+      </p>
+    </section>
+  );
+};
+
+export default About;
